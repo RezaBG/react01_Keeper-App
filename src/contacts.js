c69cd9a7387bfd4aaf9243327d9893e18c7ff0df
rezaBG <rezabarzegargashti@gmail.com>
2023-04-02 22:12:09 +0200

    Add existing files

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Tom Cruise",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/1/11/Tom_Cruise_May_2022_%28cropped%29.jpg ",
+    phone: "+123 456 789",
+    email: "b@beyonce.come",
+  },
+  {
+    name: "Michelle Monaghan",
+    imgURL:
+      "https://www.google.com/url?sa=i&url=http%3A%2F%2Ft1.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcR-3wn-IcLD5htHwsDxQ08OzvLB2-EJJQK4J5NgA0NoTuYIFDIhpILFGXPZrMlWCi62wvSUBVTYmehEIX0&psig=AOvVaw2bv3OLqi6Q9_0ED6VQBoTs&ust=1680522761775000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCODZrqORi_4CFQAAAAAdAAAAABAE",
+    phone: "+789 123 456 ",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: " Simon Pegg",
+    imgURL:
+      "https://www.google.com/url?sa=i&url=http%3A%2F%2Ft3.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcTIZ1TS10SundE7rZqepehiTmHEUmkExqY9L6PFF4otJPa-PsU5x2r69FIEDKYXqOSMNxt9xB9UC33MM2k&psig=AOvVaw2_VsPuBXoe-u9_xu5ceNc1&ust=1680522764327000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPDKyqSRi_4CFQAAAAAdAAAAABAE",
+    phone: "+456 123 789",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
